df784138cc4f062830ef3b47816ab62d214f0f8a
Austin Cory Bart <acbart@vt.edu>
2018-06-15 00:33:43 -0400

    Improve documentation

--- tifa/tifa.py
+++ tifa/tifa.py
@@ -20,6 +20,9 @@ Additionally, it reads the following as issues:
 import ast
 
 class Type:
+    '''
+    Parent class for all other types, used to provide a common interface.
+    '''
     pass
 
 class UnknownType(Type):
@@ -37,13 +40,37 @@ class RecursedType(Type):
     pass
 
 class LiteralValue:
+    '''
+    A special literal representation of a value, used to represent access on
+    certain container types.
+    '''
     pass
     
 class LiteralNum(LiteralValue):
+    '''
+    Used to capture indexes of containers.
+    '''
     def __init__(self, value):
         self.value = value
         
 class Identifier:
+    '''
+    A representation of an Identifier, encapsulating its current level of
+    existence, scope and State.
+    
+    Attributes:
+        exists (bool): Whether or not the variable actually is defined anywhere.
+                       It is possible that a variable was retrieved that does
+                       not actually exist yet, which indicates it might need to
+                       be created.
+        in_scope (bool): Whether or not the variable exists in the current
+                         scope. Used to detect the presence of certain kinds
+                         of errors where the user is using a variable from
+                         a different scope.
+        scoped_name (str): The fully qualified name of the variable, including
+                           its scope chain.
+        state (State): The current state of the variable.
+    '''
     def __init__(self, exists, in_scope=False, scoped_name="UNKNOWN", state=""):
         self.exists = exists
         self.in_scope = in_scope
@@ -51,6 +78,31 @@ class Identifier:
         self.state = state
     
 class State:
+    '''
+    A representation of a variable at a particular point in time of the program.
+    
+    Attributes:
+        name (str): The name of the variable, without its scope chain
+        trace (list of State): A recursive definition of previous States for
+                               this State.
+        type (Type): The current type of this variable.
+        method (str): One of 'store', 'read', (TODO). Indicates the change that
+                      occurred to this variable at this State.
+        position (dict): A Position dictionary indicating where this State
+                         change occurred in the source code.
+        read (str): One of 'yes', 'no', or 'maybe'. Indicates if this variable
+                    has been read since it was last changed. If merged from a
+                    diverging path, it is possible that it was "maybe" read.
+        set (str): One of 'yes', 'no', or 'maybe'. Indicates if this variable
+                    has been set since it was last read. If merged from a 
+                    diverging path, it is possible that it was "maybe" changed.
+        over (str): One of 'yes', 'no', or 'maybe'. Indicates if this variable
+                    has been overwritten since it was last set. If merged from a 
+                    diverging path, it is possible that it was "maybe" changed.
+        over_position (dict): A Position indicating where the State was
+                              previously set versus when it was overwritten.
+        
+    '''
     def __init__(self, name, trace, type, method, position, 
                  read='maybe', set='maybe', over='maybe', over_position=None):
         self.name = name
@@ -64,12 +116,21 @@ class State:
         self.over = over
     
     def copy(self, method, position):
+        '''
+        Make a copy of this State, copying this state into the new State's trace
+        '''
         return State(self.name, [self], self.type, method, position,
                      state.read, state.set, state.over, state.over_position)
 
     def __str__(self):
+        '''
+        Create a string representation of this State.
+        '''
         return self.method+"("+"|".join((self.read, self.set, self.over))+")"
     def __repr__(self):
+        '''
+        Create a string representation of this State.
+        '''
         return str(self)
                      
 class Tifa(ast.NodeVisitor):
@@ -85,6 +146,9 @@ class Tifa(ast.NodeVisitor):
     
     @staticmethod
     def _initialize_report():
+        '''
+        Return a successful report with possible set of issues.
+        '''
         return {"success": True,
                 "variables": {},
                 "issues": {
@@ -121,11 +185,23 @@ class Tifa(ast.NodeVisitor):
         }
     
     def report_issue(self, issue, data):
+        '''
+        Report the given issue with associated metadata, including the position
+        if not explicitly included.
+        '''
         if 'position' not in data:
             data['position'] = self.locate()
         self.report.issues[issue].append(data)
         
     def locate(self):
+        '''
+        Return a dictionary representing the current location within the
+        AST.
+        
+        Returns:
+            Position dict: A dictionary with the fields 'column' and 'line',
+                           indicating the current position in the source code.
+        '''
         node = self.node_chain[-1]
         return {'column': node.col_offset, 'line': node.lineno}
                 
@@ -159,6 +235,8 @@ class Tifa(ast.NodeVisitor):
         
         Args:
             ast (Ast): The AST object
+        Returns:
+            Report: The final report object created (also available as a field).
         '''
         self._reset()
         # Initialize a new, empty report
@@ -177,6 +255,10 @@ class Tifa(ast.NodeVisitor):
         return self.report
     
     def _collect_top_level_varaibles(self):
+        '''
+        Walk through the variables and add any at the top level to the
+        top_level_variables field of the report.
+        '''
         self.report['top_level_variables'] = {}
         main_path_vars = self.name_map[self.path_chain[0]]
         for full_name in main_path_vars:
@@ -207,16 +289,30 @@ class Tifa(ast.NodeVisitor):
         self.path_parents = {}
         
     def find_variable_scope(self, name):
-        for j, scope in enumerate(self.scope_chain):
+        '''
+        Walk through this scope and all enclosing scopes, finding the relevant
+        identifier given by `name`.
+        
+        Returns:
+            Identifier: An Identifier for the variable, which could potentially
+                        not exist.
+        '''
+        for scope_index, scope in enumerate(self.scope_chain):
             for path_id in self.path_chain:
                 path = self.name_map[path_id]
-                full_name = "/".join(map(str, self.scope_chain[:j]))+"/"+name
+                full_name = "/".join(map(str, self.scope_chain[:scope_index]))+"/"+name
                 if full_name in path:
-                    return Identifier(True, j==0, full_name, path[full_name])
+                    is_root_scope = (scope_index==0)
+                    return Identifier(True, is_root_scope, 
+                                      full_name, path[full_name])
                         
         return Identifier(False)
         
     def _finish_scope(self):
+        '''
+        Walk through all the variables present in this scope and ensure that
+        they have been read and not overwritten.
+        '''
         path_id = self.path_chain[0];
         for name in self.name_map[path_id]:
             if Tifa.sameScope(name, self.scope_chain):
@@ -232,6 +328,11 @@ class Tifa(ast.NodeVisitor):
     def visit(self, node):
         '''
         Process this node by calling its appropriate visit_*
+        
+        Args:
+            node (AST): The node to visit
+        Returns:
+            Type: The type calculated during the visit.
         '''
         # Start processing the node
         self.node_chain.append(node)
@@ -258,15 +359,42 @@ class Tifa(ast.NodeVisitor):
             return result
             
     def _visit_nodes(self, nodes):
+        '''
+        Visit all the nodes in the given list.
+        
+        Args:
+            nodes (list): A list of values, of which any AST nodes will be
+                          visited.
+        '''
         for node in nodes:
             if isinstance(node, ast.AST):
                 self.visit(node)
                 
     def walk_targets(self, targets, type, walker):
+        '''
+        Iterate through the targets and call the given function on each one.
+        
+        Args:
+            targets (list of Ast nodes): A list of potential targets to be
+                                         traversed.
+            type (Type): The given type to be unraveled and applied to the
+                         targets.
+            walker (Ast Node, Type -> None): A function that will process
+                                             each target and unravel the type.
+        '''
         for target in targets:
             walker(target, type)
             
     def visit_Assign(self, node):
+        '''
+        Simple assignment statement:
+        __targets__ = __value__
+        
+        Args:
+            node (AST): An Assign node
+        Returns:
+            None
+        '''
         # Handle value
         value_type = self.visit(node.value);
         # Handle targets
@@ -286,9 +414,26 @@ class Tifa(ast.NodeVisitor):
         self.walk_targets(node.targets, value_type, action)
         
     def _scope_chain_str(self):
+        '''
+        Convert the current scope chain to a string representation (divided 
+        by "/").
+        
+        Returns:
+            str: String representation of the scope chain.
+        '''
         return "/".join(map(str, self.scope_chain))
         
     def store_variable(self, name, type):
+        '''
+        Update the variable with the given name to now have the new type.
+        
+        Args:
+            name (str): The unqualified name of the variable. The variable will
+                        be assumed to be in the current scope.
+            type (Type): The new type of this variable.
+        Returns:
+            State: The new state of the variable.
+        '''
         full_name = self._scope_chain_str() + "/" + name
         current_path = self.path_chain[0]
         variable = self.find_variable_scope(name)
@@ -315,7 +460,7 @@ class Tifa(ast.NodeVisitor):
                 new_state.set = 'yes'
                 new_state.read = 'no'
             self.name_map[current_path][full_name] = new_state
-        return new_state;
+        return new_state
     
     @staticmethod
     def index_sequence_type(type, i=0):
