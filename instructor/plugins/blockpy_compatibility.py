4011be773c29ec7546d55ebe72c9347b9044924b
Austin Cory Bart <acbart@vt.edu>
2018-07-12 09:03:25 -0400

    Setting up blockpy support

--- instructor/plugins/blockpy_compatibility.py
+++ instructor/plugins/blockpy_compatibility.py
@@ -1,2 +1,110 @@
+class GracefulExit(Exception):
+    pass
+
+def set_success():
+    pass
+    
+def compliment(message):
+    pass
+    
+def give_partial(value, message):
+    pass
+    
+def hide_correctness():
+    pass
+    
+def explain(message, priority, line):
+    pass
+
+def gently(message, line):
+    pass
+
+def suppress(type, subtype):
+    pass
+    
+def log(message):
+    pass
+    
+def debug(message):
+    pass
+    
+def log_ast():
+    pass
+    
+def log_variables():
+    pass
+    
+def log_behavior():
+    pass
+    
+def log_issues():
+    pass
+    
+def log_trace():
+    pass
+    
+class StudentData:
+    def __init__(self):
+        pass
+    def get_names_by_type(self, type, exclude_builtins):
+        pass
+    def get_values_by_type(self, type, exclude_builtins):
+        pass
+    
+student = StudentData()
 def get_output():
     pass
+def reset_output():
+    pass
+def queue_input(*inputs):
+    pass
+def get_program():
+    pass
+def parse_program():
+    pass
+def had_execution_time_error():
+    pass
+def limit_execution_time():
+    pass
+def unlimit_execution_time():
+    pass
+def analyze_program():
+    pass
+
+def def_use_error(AstNode):
+    pass
+
+class CorruptedAstNode:
+    def __init__(self):
+        pass
+def find_match(instructor_code):
+    pass
+def find_matches(instructor_code):
+    pass
+
+class ASTMap:
+    def __init__(self, JSAstMap):
+        pass
+    def get_std_name(self, id):
+        pass
+    def get_std_exp(self, id):
+        pass
+    
+class AstNode:
+    def __init__(self, id):
+        pass
+    def __eq__(self, other):
+        pass
+    def numeric_logic_check(self, mag, expr):
+        pass
+    def __str__(self):
+        pass
+    def __repr__(self):
+        pass
+    def __getattr__(self, key):
+        pass
+    def has(self, AstNode):
+        pass
+    def find_all(self, type):
+        pass
+    
--- instructor/plugins/grade_magic.py
+++ instructor/plugins/grade_magic.py
@@ -2,6 +2,19 @@ from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 from IPython.display import Javascript, display
 import json
 
+from requests import get
+
+BLOCKPY_URL = 'https://think.cs.vt.edu/blockpy/'
+
+def blockpy_grade(assignment_id, student_code):
+    data = {'assignment_id': assignment_id}
+    response = get(BLOCKPY_URL+'load_assignment_give_feedback', data=data)
+    result = response.json()
+    if result['success']:
+        return exec(result['give_feedback'])
+    else:
+        return ""
+
 EMBED_STUDENT_CODE = r'''
 var source_code = Jupyter.notebook.get_cells().map(function(cell) {
     if (cell.cell_type == "code") {
@@ -17,8 +30,8 @@ var instructor_code = "student_code="+source_code+"\n";
 LOCAL_GRADE = r'''instructor_code += {code}
 console.log(instructor_code);'''
 BLOCKPY_GRADE = r'''//instructor_code += "\nimport ast\nprint(ast.dump(ast.parse(student_code)))\nprint('Great')"
-instructor_code += "from instructoranalysis import plugins\n";
-instructor_code += "result=plugins.blockpy_grade({assignment}, student_code);\n"
+instructor_code += "from instructor.plugins.grade_magic import blockpy_grade\n";
+instructor_code += "result=blockpy_grade({assignment}, student_code);\n"
 console.log(instructor_code);'''
 EXECUTE_CODE = r'''
 var kernel = IPython.notebook.kernel;
