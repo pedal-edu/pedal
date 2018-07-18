17db43611f0f9c686eada3b131e2323ab53d4b5e
Austin Cory Bart <acbart@vt.edu>
2018-07-18 09:29:37 -0400

    Report and example

--- pedal/report/__init__.py
+++ pedal/report/__init__.py
@@ -1,30 +1,30 @@
 from pedal.report.report import Report
 
-_MAIN_REPORT = Report()
+MAIN_REPORT = Report()
 
 def set_success():
-    _MAIN_REPORT.set_success()
+    MAIN_REPORT.set_success()
     
 def compliment(message):
-    _MAIN_REPORT.compliment(message)
+    MAIN_REPORT.compliment(message)
     
 def give_partial(value, message):
-    _MAIN_REPORT.give_partial(value, message)
+    MAIN_REPORT.give_partial(value, message)
     
 def hide_correctness():
-    _MAIN_REPORT.hide_correctness()
+    MAIN_REPORT.hide_correctness()
     
 def explain(message, priority, line):
-    _MAIN_REPORT.explain(message, priority, line)
+    MAIN_REPORT.explain(message, priority, line)
 
 def gently(message, line):
-    _MAIN_REPORT.gently(message, line)
+    MAIN_REPORT.gently(message, line)
 
 def suppress(type, subtype):
-    _MAIN_REPORT.suppress(type, subtype)
+    MAIN_REPORT.suppress(type, subtype)
     
 def log(message):
-    _MAIN_REPORT.log(message)
+    MAIN_REPORT.log(message)
     
 def debug(message):
-    _MAIN_REPORT.debug(message)
+    MAIN_REPORT.debug(message)
--- tests/example_pipeline.py
+++ tests/example_pipeline.py
@@ -1,31 +1,42 @@
 ################################################################################
 # BlockPy Examples
-# In JS, patch `get_program`
+# Boiler plate code automatically prepended before execution
+# Import get_output, set_success, gently, explain
 from pedal.report import *
-    # get_output, set_success, gently, explain
+# In JS, patch `get_program`, and then imperative define the report's source
+report.set_source(get_program())
+# Run the tifa analysis; stuff is automatically added to the report
 from pedal.tifa import tifa_analysis()
-    # tifa_analysis
-from pedal.mistakes import mistakes
-    # misconceptions
-tifa_analysis()
+_tifa = tifa_analysis()
 
+# Set up CAIT to know about TIFA
+import pedal.cait as cait
+cait.configure(tifa=_tifa)
+
+# End of boiler plate
+
+# Minor modifications to pathing for the instructor_* stuff
+from pedal.mistakes import mistakes
 mistakes.filter_group()
 mistakes.append.append_group()
 
+# But most code left unchanged
 if 'text' in get_output():
     gently("Hey now...")
 set_success()
 
+# Boilerplate begins again.
 import pedal.feedback as feedback
 _RESULT = feedback.select()
 _PARTIALS = sum_partials()
     # Runs the feedback dispatcher, figures out what the HTML string is
     # Have to access the _RESULT from 
-    # Calls blockpy.set_feedback(result)
+    # Calls blockpy.set_feedback(_RESULT, _PARTIALS)
     
 ################################################################################
 # Jupyter Notebooks
 from pedal.report import *
+report.set_source(STUDENT_SOURCE)
     # get_output, set_success, gently, explain
 from pedal.tifa import tifa_analysis()
     # tifa_analysis
