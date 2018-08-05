9c4bbb9a3fd4ac4fabb14d3aedccffc875792677
lukesg08 <lukesg08@vt.edu>
2018-08-05 00:50:40 -0400

    Added tifa_analysis to mistake_test_template finished tests for append mistakes and updated instructor append to use matching api

--- tests/mistake_test_template.py
+++ tests/mistake_test_template.py
@@ -2,6 +2,7 @@ import unittest
 from pedal.cait.cait_api import *
 from pedal.report import MAIN_REPORT
 from pedal.source import set_source
+from pedal.tifa import tifa_analysis
 
 
 class MistakeTest(unittest.TestCase):
@@ -10,3 +11,4 @@ class MistakeTest(unittest.TestCase):
         MAIN_REPORT.clear()
         set_source(source)
         parse_program()
+        tifa_analysis()
