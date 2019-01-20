f7c9ffb881f517d9837c703e4b0575082182eb67
Austin Cory Bart <acbart@vt.edu>
2019-01-20 13:39:09 -0500

    signature return typecheck, code trimming for docstring.py

---
+++ tests/sandbox_coverage/bad_non_recursive.py
@@ -0,0 +1,5 @@
+def x(n):
+    if n > 0:
+        return n-1
+    return 0
+x(5)
---
+++ tests/sandbox_coverage/complex.py
@@ -0,0 +1,16 @@
+x = [1, 2, 3]
+a = 0
+for i in x:
+    if a == 0:
+        a = 0 
+    if a:
+        c = 0
+    if a:
+        b = 0
+if False:
+    if False:
+        with open('a') as out:
+            def x():
+                class Y:
+                    import name
+a = 0
