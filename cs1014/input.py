082c76b4315bb675176b4b64369c483ba44cc080
lukesg08 <lukesg08@vt.edu>
2019-02-25 16:25:25 -0500

    Adding new mistake for fill in the blank keys. Adding labels to explain_r calls

--- cs1014/input.py
+++ cs1014/input.py
@@ -13,11 +13,13 @@ def unnecessary_cast(needed_casts):
     """
     message = "Converting to {} is unnecessary in this problem"
     code = "ex_cast"
+    tldr = "Unnecessary Conversion"
+
     known_casts = ["float", "int", "str"]
     matches = find_matches("_cast_(___)")
     for match in matches:
         user_cast = match["_cast_"].id
         if user_cast not in needed_casts and user_cast in known_casts:
-            return explain_r(message.format(user_cast), code)
+            return explain_r(message.format(user_cast), code, tldr)
     return False
 
--- cs1014/tests/test_dictionary.py
+++ cs1014/tests/test_dictionary.py
@@ -290,6 +290,26 @@ class DictionaryMistakeTest(MistakeTest):
         ret = missing_key(keys)
         self.assertFalse(ret, "Expected False, got {} instead".format(ret))
 
+    def test_blank_key(self):
+        keys = ["distance", "time"]
+        self.to_source('distance_in_kilometers = trip_data["____"]/1000\n'
+                       'trip_data = {"distance":123000.0, "time":14000.0}\n'
+                       'print(average_speed_in_mph) \n'
+                       'average_speed_in_mph = ____ / time_in_hours\n'
+                       'time_in_hours = trip_data["____"]/____\n'
+                       '____ = distance_in_kilometers / 1.6\n')
+        ret = blank_key(keys)
+        self.assertTrue(ret, "Didn't give message, returned {} instead".format(ret))
+
+        self.to_source('trip_data = {"distance":123000.0, "time":14000.0}\n'
+                       'distance_in_kilometers = trip_data["distance"]/1000\n'
+                       'distance_in_miles = distance_in_kilometers / 1.6\n'
+                       'time_in_hours = trip_data["time"]/3600\n'
+                       'average_speed_in_mph = distance_in_miles / time_in_hours\n'
+                       'print(average_speed_in_mph) \n')
+        ret = blank_key(keys)
+        self.assertFalse(ret, "Expected False, got {} instead".format(ret))
+
     def test_dict_parens_brack(self):
         # TODO: Check output string
         self.to_source('import weather\n'
