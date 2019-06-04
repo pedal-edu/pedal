0da7390f5be25f2b27b54287da57c5364fd188a4
lukesg08 <lukesg08@vt.edu>
2019-06-04 10:30:22 -0400

    updated cmd_line.py to use progsnap2

--- pedal/toolkit/cmd_line.py
+++ pedal/toolkit/cmd_line.py
@@ -4,6 +4,8 @@ from pedal.source import set_source
 from pedal.tifa import tifa_analysis
 from pedal.sandbox.compatibility import *
 import importlib.util
+import numpy as np
+import pandas as pd
 
 import sys
 import os
@@ -28,15 +30,37 @@ def setup(student_code, input_vals):
     return get_sandbox()
 
 
+def process(file, module, ins_code, report):
+    student_code1 = file.read()
+    setup(student_code1, inputs)  # setup returns a sandbox object
+    module.loader.exec_module(ins_code)
+    feedback = report.feedback
+    return feedback
+
+
+p2Flag = True
+secrets = False
 if __name__ == "__main__":
     # processing args
     feedback_code = sys.argv[1]
     code_dir = sys.argv[2]
-    inputs = sys.argv[3:]
+    flag = sys.argv[3]
+    if flag == "-p2":
+        p2Flag = True
+        inputs = sys.argv[4:]
+    elif flag == "-secrets":
+        p2Flag = True
+        secrets = True
+        inputs = sys.argv[4:]
+    else:
+        inputs = sys.argv[3:]
 else:
     feedback_code = ("C:/Users/User/Documents/Luke_Stuff/Research/ComputationalThinking/DictionaryUnit/test_cmd/"
                      "ins_script.py")
-    code_dir = "C:/Users/User/Documents/Luke_Stuff/Research/ComputationalThinking/DictionaryUnit/test_cmd/students/"
+    code_dir = ("C:/Users/User/Documents/Luke_Stuff/Research/ComputationalThinking/ResearchData/"
+                "ComputationalThinking/Tests/results/Spring2019/DictionaryData/cs1014_spr2019_log-v1/")
+    p2Flag = True
+    secrets = True
     inputs = []
 
 # Grabbing instructor feedback code
@@ -44,21 +68,34 @@ ins_mod = re.match("(?:.*/)(.*).py", feedback_code)[1]
 my_spec = importlib.util.spec_from_file_location(ins_mod, feedback_code)
 foo = importlib.util.module_from_spec(my_spec)
 
-# Grabbing folder of student files
-print(os.getcwd())
-student_files = os.listdir(code_dir)
-
 # preparing to process
-student_feedback = []
-report = MAIN_REPORT
 
-for code_name in student_files:
-    with open(code_dir + code_name) as code_f:
-        student_code1 = code_f.read()
-        student = setup(student_code1, inputs)
-        my_spec.loader.exec_module(foo)
-        feedback = report.feedback
-        student_feedback.append(feedback)
 
-if __name__ == "__main__":
-    print(student_feedback)
+# Grabbing student files
+if p2Flag:
+    student_feedback = []
+    main_table = "MainTable"
+    if secrets:
+        main_table += "-2"
+    main_table += ".csv"
+    df = pd.read_csv(code_dir + main_table)
+    code_states = code_dir + "CodeStates/"
+    for index, row in df.iterrows():
+        code_f = code_states + str(int(row['CodeStateID'])) + "/__main__.py"
+        with open(code_f) as code:
+            feedback_result = process(code, my_spec, foo, MAIN_REPORT)
+            student_feedback.append(feedback_result)
+            df.at[index, 'Score'] = 0.0 if feedback_result else 1.0
+    df.to_csv(code_dir + "processed.csv", index=False)
+else:
+    student_feedback = []
+    print(os.getcwd())
+    student_files_base = os.listdir(code_dir)
+    student_files = []
+    for code_name in student_files_base:
+        student_files.append(code_dir + code_name)
+    for code_name in student_files:
+        with open(code_name) as code_f:
+            student_feedback.append(process(code_f, my_spec, foo, MAIN_REPORT))
+    if __name__ == "__main__":
+        print(student_feedback)
