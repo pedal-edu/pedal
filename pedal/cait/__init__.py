5eff02c62a871bfe0e6d964882c2331278cb318d
Austin Cory Bart <acbart@vt.edu>
2020-07-03 23:26:32 -0400

    Move imports to top of file.

--- pedal/cait/__init__.py
+++ pedal/cait/__init__.py
@@ -8,15 +8,15 @@ Optional:
 - :py:class:`pedal.tifa.tifa.Tifa`
 """
 
+from pedal.cait.cait_api import (find_match, find_matches,
+                                 parse_program,
+                                 find_submatches, find_expr_sub_matches,
+                                 def_use_error, data_state, data_type,
+                                 expire_cait_cache)
+
 NAME = 'CAIT'
 SHORT_DESCRIPTION = "Captures instructor code patterns within student code."
 DESCRIPTION = '''
 '''
 REQUIRES = ['Source']
 OPTIONALS = ['TIFA']
-
-from pedal.cait.cait_api import (find_match, find_matches,
-                                 parse_program,
-                                 find_submatches, find_expr_sub_matches,
-                                 def_use_error, data_state, data_type,
-                                 expire_cait_cache)
