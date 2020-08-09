e910a393aeca29c40fcfb8c2fd9099008fd40031
Austin Cory Bart <acbart@vt.edu>
2020-08-09 02:12:11 -0400

    New default resolver: print_resolve.

--- pedal/resolvers/__init__.py
+++ pedal/resolvers/__init__.py
@@ -18,3 +18,19 @@ Full Summary
     Report all feedback but divided into frequencies of labels grouped by tool/category/priority/time.
 
 """
+
+from pedal.resolvers import simple
+
+
+def print_resolve(*args, **kwargs):
+    """
+    Trivial formatter for resolver, just dumps the
+    Title/Label/Score/Message. Any arguments are forwarded to
+    :py:func:`pedal.resolvers.simple.resolve`
+    """
+    result = simple.resolve(*args, **kwargs)
+    print("Title:", result.title)
+    print("Label:", result.label)
+    print("Score:", result.score)
+    print("Message:", result.message)
+    return result
