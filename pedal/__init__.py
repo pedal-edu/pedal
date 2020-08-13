3a5633a8f7ee4b5af1afc8fed74cfaf5d1c37482
Austin Cory Bart <acbart@vt.edu>
2020-08-13 16:00:39 -0400

    Import cait automatically for pedal.

--- pedal/__init__.py
+++ pedal/__init__.py
@@ -10,6 +10,7 @@ from pedal.core.submission import Submission
 from pedal.core.commands import *
 from pedal.source import *
 from pedal.sandbox.commands import *
+from pedal.cait import *
 from pedal.assertions.commands import *
 
 student: Sandbox
