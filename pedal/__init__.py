8d86d7fdf20cdead58d8f70f6f8976df33cc330f
Austin Cory Bart <acbart@vt.edu>
2020-08-14 13:35:54 -0400

    Kill fancy machinery that let's you run instructor scripts as a main file. It just wasn't working often enough. Now you have to run via Pedal.

--- pedal/__init__.py
+++ pedal/__init__.py
@@ -18,37 +18,3 @@ student: Sandbox
 # Am I being run as Pedal, or via Python?
 # Check if an environment was set via CLI
 # Load in a submission file if it was given
-
-# Did the user pass in our special flag or set an environment variable?
-PEDAL_AS_LIBRARY_FLAG = '--PEDAL_AS_LIBRARY' in sys.argv
-try:
-    PEDAL_AS_LIBRARY_FLAG = os.environ['PEDAL_AS_LIBRARY']
-except (AttributeError, KeyError):
-    pass
-
-def is_common_tool(command_line_context):
-    return command_line_context.endswith('pydevconsole.py')
-
-# If we get command line arguments and we're not told to be a library...
-if sys.argv and not PEDAL_AS_LIBRARY_FLAG:
-    # Only import if we HAVE sysargs
-    from pedal.command_line.command_line import main, parse_args
-    command_line_context = sys.argv[0]
-    # Running this as a module
-    if command_line_context == "-m":
-        # Emulate the console_script mode
-        pass
-    # Running this via the console_script ``pedal``
-    elif command_line_context.endswith('pedal'):
-        # We get to do all the hijinx!
-        pass
-    # Otherwise, assume Python file that has the complete instructor grading script.
-    elif is_common_tool(command_line_context):
-        pass
-    else:
-        # We need to just use the parameters to set things up, don't run.
-        # Need to setup environment, and make appropriate variables available.
-        if len(sys.argv) > 1:
-            args = parse_args(True)
-            args.instructor = sys.argv[0]
-            main(args)
