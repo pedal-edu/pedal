6286b81be46a1446ce2ba86514b602ce494ee5a3
Austin Cory Bart <acbart@vt.edu>
2024-03-19 12:07:42 -0400

    Drafter improvements

--- pedal/types/library/drafter.py
+++ pedal/types/library/drafter.py
@@ -1,12 +1,14 @@
 from pedal.types.new_types import ModuleType, FunctionType, void_function, ClassType, \
-    InstanceType, bool_function, int_function, str_function, register_builtin_module
+    InstanceType, bool_function, int_function, str_function, register_builtin_module, AnyType, ListType
 
 # TODO: route, Button, ...
 
-_VOID_FUNCTIONS = []
-_DESIGNER_FUNCTIONS = []
-_IDENTITY_FUNCTIONS = []
-_DECORATOR_FUNCTIONS = ["route"]
+_VOID_FUNCTIONS = ['start_server', 'show_debug_information', 'hide_debug_information']
+_COMPONENT_FUNCTIONS = ["Image", "Link", "TextBox", "TextArea", "SelectBox",
+                        "CheckBox", "LineBreak", "HorizontalRule", "Button",
+                        "NumberedList", "BulletedList", "Header", "Table"]
+_IDENTITY_FUNCTIONS = ["route"]
+
 
 def build_drafter_module():
     _PageObject = ClassType('Page', {
@@ -14,32 +16,26 @@ def build_drafter_module():
         'content': ListType(AnyType())
     }, [])
 
-    def _DesignerObject_constructor():
-        return InstanceType(_DesignerObject)
+    def _PageObject_Constructor():
+        return InstanceType(_PageObject)
 
-    _DESIGNER_FIELDS = {
-        'colliding': bool_function('colliding'),
-        'colliding_with_mouse': bool_function('colliding_with_mouse'),
-        'get_window_title': str_function('get_window_title'),
-        'get_mouse_x': int_function('get_mouse_x'),
-        'get_mouse_y': int_function('get_mouse_y'),
-        'get_height': int_function('get_height'),
-        'get_width': int_function('get_width'),
-        'get_keyboard_delay': int_function('get_keyboard_delay'),
-        'get_keyboard_interval': int_function('get_keyboard_interval'),
-        'get_keyboard_repeat': bool_function('get_keyboard_repeat'),
-        'get_emoji_name': str_function('get_emoji_name'),
-        'get_text': str_function('get_text'),
+    _ComponentObject = ClassType('Component', {}, [])
+
+    def _ComponentObject_Constructor():
+        return InstanceType(_ComponentObject)
+
+    _DRAFTER_FIELDS = {
+        'Page': FunctionType('Page', returns=_PageObject_Constructor),
     }
 
-    _DESIGNER_FIELDS.update({
+    _DRAFTER_FIELDS.update({
         name: void_function(name) for name in _VOID_FUNCTIONS
     })
-    _DESIGNER_FIELDS.update({
-        name: FunctionType(name, returns=_DesignerObject_constructor)
-        for name in _DESIGNER_FUNCTIONS
+    _DRAFTER_FIELDS.update({
+        name: FunctionType(name, returns=_ComponentObject_Constructor)
+        for name in _COMPONENT_FUNCTIONS
     })
-    _DESIGNER_FIELDS.update({
+    _DRAFTER_FIELDS.update({
         name: FunctionType(name, returns='identity')
         for name in _IDENTITY_FUNCTIONS
     })
