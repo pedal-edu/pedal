efa4e107dca5450ab44bdc82318cecfc5c160740
Austin Cory Bart <acbart@vt.edu>
2024-03-21 07:36:53 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pedal',
-    version='2.6.0',
+    version='2.6.1',
     python_requires='>=3.7',
     author='acbart,lukesg08',
     author_email='acbart@udel.edu',
