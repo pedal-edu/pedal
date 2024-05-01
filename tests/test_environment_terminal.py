76666ff387dd29ab6727a0ee6f47b84ac7083899
Austin Cory Bart <acbart@vt.edu>
2024-05-01 13:03:10 -0400

    Update unit tests for spaces in terminal header environment

--- tests/test_environment_terminal.py
+++ tests/test_environment_terminal.py
@@ -30,10 +30,10 @@ class TestEnvironmentTerminal(ExecutionTestCase):
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;92m✓[0;0m️  Your code ran successfully.
 
@@ -46,10 +46,10 @@ Great work! Based on your code submitted, there are no other recommendations ava
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  No Source Code
 
@@ -62,17 +62,17 @@ Source code file is blank.
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  Syntax Error
 
 Bad syntax on line [4m1[0;0m.
 Suggestion: Check line [4m1[0;0m, the line before it, and the line after it. Ignore blank lines.
 
-[;7mTERMINAL OUTPUT[0;0m For reference, here is the original Python error:
+[;7m TERMINAL OUTPUT [0;0m For reference, here is the original Python error:
 
 Line [4m1[0;0m of file [4manswer.py[0;0m
     1 1 2 3
@@ -86,16 +86,16 @@ SyntaxError: Invalid syntax.
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  Incompatible types
 
 You used an addition operation with an integer and a string on line [4m1[0;0m. But you can't do that with that operator. Make sure both sides of the operator are the right type.
 
-[;7mTERMINAL OUTPUT[0;0m For reference, here is the original Python error:
+[;7m TERMINAL OUTPUT [0;0m For reference, here is the original Python error:
 
 Line [4m1[0;0m of file [4manswer.py[0;0m
     1+''
@@ -109,10 +109,10 @@ TypeError: Unsupported operand type(s) for +: 'int' and 'str'
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  Type Error
 
@@ -126,7 +126,7 @@ Type errors occur when you use an operator or function on the wrong type of valu
 
 Suggestion: To fix a type error, you should trace through your code. Make sure each expression has the type you expect it to have.
 
-[;7mTERMINAL OUTPUT[0;0m For reference, here is the original Python error:
+[;7m TERMINAL OUTPUT [0;0m For reference, here is the original Python error:
 
 Line [4m2[0;0m of file [4manswer.py[0;0m
     json.loads('1')+''
@@ -141,16 +141,16 @@ TypeError: Unsupported operand type(s) for +: 'int' and 'str'
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  Instructor Feedback
 
 You should not be using the JSON library at all.
 
-[;7mTERMINAL OUTPUT[0;0m For reference, here is the original Python error:
+[;7m TERMINAL OUTPUT [0;0m For reference, here is the original Python error:
 
 Line [4m2[0;0m of file [4manswer.py[0;0m
     json.loads('1')+''
@@ -166,10 +166,10 @@ TypeError: Unsupported operand type(s) for +: 'int' and 'str'
             terminal.resolve()
             clear_report()
             self.checkFeedback(f.getvalue(), '''
-[;7mYOUR CODE[0;0m We ran your code. Here's the output:
+[;7m YOUR CODE [0;0m We ran your code. Here's the output:
 
 
-[;7mFEEDBACK[0;0m Based on your code, here are some tips and recommendations:
+[;7m FEEDBACK [0;0m Based on your code, here are some tips and recommendations:
 
 [1;91m✗[0;0m  Type Error
 
@@ -184,7 +184,7 @@ Type errors occur when you use an operator or function on the wrong type of valu
 
 Suggestion: To fix a type error, you should trace through your code. Make sure each expression has the type you expect it to have.
 
-[;7mTERMINAL OUTPUT[0;0m For reference, here is the original Python error:
+[;7m TERMINAL OUTPUT [0;0m For reference, here is the original Python error:
 
 Line [4m1[0;0m of file [4manswer.py[0;0m in x
     def x(): return 1+''
